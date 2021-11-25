700d6aa4526a3623f78e54b87d35f60c40329b7c
hendrik wagenseil <hendrik.wagenseil@metergroup.com>
2021-11-25 08:32:51 +0100

    add svg-files to gitignore, update osm map creation, add references and things to explore

---
+++ R/dev_history.R
@@ -0,0 +1,6 @@
+## 2021-11-25 ----
+#
+# check out:
+# - rayshader: https://www.rayshader.com/
+# - supercells: https://nowosad.github.io/supercells/
+
--- R/streetmaps.R
+++ R/streetmaps.R
@@ -1,6 +1,15 @@
-## reference ----
-# http://joshuamccrain.com/tutorials/maps/streets_tutorial.html
-
+## references ----
+# osm extraction:
+# - https://docs.ropensci.org/osmextract/
+#
+# map creation:
+# - https://ggplot2tutor.com/tutorials/streetmaps
+# - http://joshuamccrain.com/tutorials/maps/streets_tutorial.html
+# - https://github.com/lina2497/Giftmap
+#
+# scaling and dimensions:
+# - https://www.christophenicault.com/post/understand_size_dimension_ggplot2/
+# - https://www.tidyverse.org/blog/2020/08/taking-control-of-plot-scaling/
 
 ## setup ----
 library(osmdata)
@@ -9,32 +18,78 @@ library(ggplot2)
 library(ggmap)
 
 
-## data extractio ----
+## data extraction ----
+available_features()
+available_tags("highway")
+available_tags("water")
+
 bbox = getbb("Nuremberg")
 
-# available_tags("highway")
+# bbox = sf::st_bbox(
+#     c(xmin = 11.01
+#       , xmax = 11.05
+#       , ymax = 49.52
+#       , ymin = 49.54
+#     )
+# )
+#
+feat = opq(bbox) |>
+    add_osm_feature(
+        key = "water"
+        , value = available_tags("water")
+    ) |>
+    osmdata_sf()
+
+mapview::mapview(feat$osm_lines)
+mapview::mapview(feat$osm_polygons)
+# plot(sf::st_geometry(major$osm_lines))
 
 major = opq(bbox) |>
     add_osm_feature(
         key = "highway"
-        , value = c("motorway", "primary", "motorway_link", "primary_link")
+        , value = c(
+            "motorway"
+            , "motorway_link"
+            , "motorway_junction"
+            , "primary"
+            , "primary_link"
+            , "trunk"
+        )
     ) |>
     osmdata_sf()
 
 minor = opq(bbox) |>
     add_osm_feature(
         key = "highway"
-        , value = c("secondary", "tertiary", "secondary_link", "tertiary_link")) |>
+        , value = c(
+            "secondary"
+            , "tertiary"
+            , "secondary_link"
+            , "tertiary_link"
+        )
+    ) |>
     osmdata_sf()
 
 small = opq(bbox) |>
     add_osm_feature(
         key = "highway"
-        , value = c("residential", "living_street", "unclassified", "service", "footway")) |>
+        , value = c(
+            "residential"
+            , "living_street"
+            , "unclassified"
+            , "service"
+            , "footway"
+            , "corridor"
+            , "bridleway"
+        )
+    ) |>
     osmdata_sf()
 
 river = opq(bbox) |>
-    add_osm_feature(key = "waterway", value = "river") |>
+    add_osm_feature(
+        key = "waterway"
+        , value = "river"
+    ) |>
     osmdata_sf()
 
 railway = opq(bbox) |>
@@ -44,44 +99,56 @@ railway = opq(bbox) |>
 
 ## plotting ----
 font_add_google(name = "Lato", family = "lato")
-showtext_auto()
+showtext_auto(enable = TRUE)
 
-p = ggplot() +
+bg = "white"
+bg = "black"
+col = "black"
+col = "white"
+
+ggplot() +
     geom_sf(
         data = river$osm_lines
         , inherit.aes = FALSE
         , color = "steelblue"
-        , size = 5
-        , alpha = .3
+        , size = 0.8
+        , alpha = 0.6
+    ) +
+    geom_sf(
+        data = river$osm_polygons
+        , inherit.aes = FALSE
+        , fill = "steelblue"
+        # , size = 0.8
+        , alpha = 0.6
     ) +
     geom_sf(
         data = railway$osm_lines
         , inherit.aes = FALSE
-        , color = "black"
-        , size = 1.5
-        , linetype="dotdash"
-        , alpha = .5
+        , color = col
+        , size = 0.5
+        , linetype = "dotdash"
+        , alpha = 0.8
     ) +
     geom_sf(
         data = small$osm_lines
         , inherit.aes = FALSE
-        , color = "#666666"
-        , size = 1
-        , alpha = .3
+        , color = col
+        , size = 0.4
+        , alpha = 0.5
     ) +
     geom_sf(
         data = minor$osm_lines
         , inherit.aes = FALSE
-        , color = "black"
-        , size = 1.5
-        , alpha = .5
+        , color = col
+        , size = 0.4
+        , alpha = 0.6
     ) +
     geom_sf(
         data = major$osm_lines
         , inherit.aes = FALSE
-        , color = "black"
-        , size = 2
-        , alpha = .6
+        , color = col
+        , size = 0.4
+        , alpha = 0.8
     ) +
     coord_sf(
         xlim = bbox[1,]
@@ -91,32 +158,53 @@ p = ggplot() +
     theme_void() +
     theme(
         plot.title = element_text(
-            size = 80
-            , family = "lato"
+            family = "lato"
+            , size = 28
             , face = "bold"
             , hjust = .5
+            , colour = col
         )
         , plot.subtitle = element_text(
             family = "lato"
-            , size = 40
+            , size = 16
             , hjust = .5
             , margin = margin(2, 0, 5, 0)
+            , colour = col
         )
+        , plot.caption = element_text(
+            family = "lato"
+            , vjust = 10
+            , colour = col
+        )
+
     ) +
     labs(title = "Nürnberg"
          , subtitle = paste0(
              round(mean(bbox[2,]), 3), "°N / ",
              round(mean(bbox[1,]), 3), "°E"
          )
+         , caption = "© OpenStreetMap contributors"
     )
 
 ggsave(
-    "img/nuernberg.pdf"
-    , plot = p
+    paste0("img/nuernberg_", bg, ".svg")
+    , device = "svg"
+    # , plot = p
     , width = 600
     , height = 600
     , units = "mm"
     , dpi = "print"
-    , bg = "white"
+    , bg = bg
+)
+
+ggsave(
+    paste0("img/nuernberg_", bg, ".jpg")
+    , device = "jpeg"
+    # , plot = p
+    , width = 600
+    , height = 600
+    , units = "mm"
+    , dpi = "retina"
+    , bg = bg
 )
 
